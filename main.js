a32ccf9543a1ef0b556481b2c178ad8d3c7c9be2
hideMan <thanhdntage@gmail.com>
2019-11-15 16:19:08 +0700

    Class

--- main.js
+++ main.js
@@ -1,10 +1,25 @@
-var drawPoint = function (point) {
-    console.log("Draw point at X: " + point.x + " & Y: " + point.y);
-};
-var drawPointX = function (point) {
-    console.log("Draw point at X: " + point.x + " & Y: " + point.y);
-};
-drawPointX({
-    x: 100,
-    y: 2
-});
+// Interface
+// interface Point {
+//     x: number;
+//     y: number;
+// }
+// vi pham cohesion : nhung gi lien quan vs nhau thi nen nam chung vs nhau
+// ->  using Class
+var Point = /** @class */ (function () {
+    function Point(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+    Point.prototype.drawPoint = function () {
+        console.log("Draw point at X: " + this.x + " & Y: " + this.y);
+    };
+    Point.prototype.movePoint = function () {
+        console.log("Draw point at X: " + this.x + " & Y: " + this.y);
+    };
+    Point.prototype.cutPoint = function () {
+        console.log("Draw point at X: " + this.x + " & Y: " + this.y);
+    };
+    return Point;
+}());
+var point = new Point(3, 4);
+point.drawPoint();
--- main.ts
+++ main.ts
@@ -1,24 +1,35 @@
 // Interface
-interface Point {
-    x: number,
-    y: number
-}
+// interface Point {
+//     x: number;
+//     y: number;
+// }
 
-let drawPoint = (point: {x: number, y: number}) => {// Inline Annotation
-    console.log(`Draw point at X: ${point.x} & Y: ${point.y}`);  
-}
-
-let movePoint = (point: Point) => {// Inline Annotation
-    console.log(`Draw point at X: ${point.x} & Y: ${point.y}`);  
-}
-
-let cutPoint = (point: Point) => {// Inline Annotation
-    console.log(`Draw point at X: ${point.x} & Y: ${point.y}`);  
-}
-
-drawPoint({
-    x: 100,
-    y: 2
-});
 
 // vi pham cohesion : nhung gi lien quan vs nhau thi nen nam chung vs nhau
+// ->  using Class
+
+class Point { // has Access modifiers| same php
+
+    private x: number;
+    private y: number;
+
+    constructor(x: number, y: number) {
+        this.x = x;
+        this.y = y;
+    }
+
+    drawPoint() {
+        console.log(`Draw point at X: ${this.x} & Y: ${this.y}`);
+    }
+
+    movePoint() {
+        console.log(`Draw point at X: ${this.x} & Y: ${this.y}`);
+    }
+
+    cutPoint() {
+        console.log(`Draw point at X: ${this.x} & Y: ${this.y}`);
+    }
+}
+
+let point = new Point(3,4);
+point.drawPoint();
