0c11c06360d534b746861dc552503ce017b67cba
hideMan <thanhdntage@gmail.com>
2019-11-15 16:34:18 +0700

    Class clean

--- main.js
+++ main.js
@@ -6,20 +6,40 @@
 // vi pham cohesion : nhung gi lien quan vs nhau thi nen nam chung vs nhau
 // ->  using Class
 var Point = /** @class */ (function () {
-    function Point(x, y) {
-        this.x = x;
-        this.y = y;
+    function Point(_x, _y) {
+        this._x = _x;
+        this._y = _y;
     }
     Point.prototype.drawPoint = function () {
-        console.log("Draw point at X: " + this.x + " & Y: " + this.y);
+        console.log("Draw point at X: " + this._x + " & Y: " + this._y);
     };
     Point.prototype.movePoint = function () {
-        console.log("Draw point at X: " + this.x + " & Y: " + this.y);
+        console.log("Draw point at X: " + this._x + " & Y: " + this._y);
     };
     Point.prototype.cutPoint = function () {
-        console.log("Draw point at X: " + this.x + " & Y: " + this.y);
+        console.log("Draw point at X: " + this._x + " & Y: " + this._y);
     };
+    Object.defineProperty(Point.prototype, "getX", {
+        // Getters & Setters
+        get: function () {
+            return this._x;
+        },
+        enumerable: true,
+        configurable: true
+    });
+    Object.defineProperty(Point.prototype, "setX", {
+        set: function (value) {
+            if (value < 0) {
+                throw new Error('Value cannot be less than 0');
+            }
+            this._x = value;
+        },
+        enumerable: true,
+        configurable: true
+    });
     return Point;
 }());
 var point = new Point(3, 4);
+point.setX = 199;
+console.log(point.getX); // correct <> getX() wrong
 point.drawPoint();
--- main.ts
+++ main.ts
@@ -10,26 +10,35 @@
 
 class Point { // has Access modifiers| same php
 
-    private x: number;
-    private y: number;
-
-    constructor(x: number, y: number) {
-        this.x = x;
-        this.y = y;
-    }
+    constructor(private _x: number, private _y: number) {}
 
     drawPoint() {
-        console.log(`Draw point at X: ${this.x} & Y: ${this.y}`);
+        console.log(`Draw point at X: ${this._x} & Y: ${this._y}`);
     }
 
     movePoint() {
-        console.log(`Draw point at X: ${this.x} & Y: ${this.y}`);
+        console.log(`Draw point at X: ${this._x} & Y: ${this._y}`);
     }
 
     cutPoint() {
-        console.log(`Draw point at X: ${this.x} & Y: ${this.y}`);
+        console.log(`Draw point at X: ${this._x} & Y: ${this._y}`);
+    }
+
+    // Getters & Setters
+    get getX() {
+        return this._x;
+    }
+
+    set setX(value: number) {
+        if (value < 0) {
+            throw new Error('Value cannot be less than 0');
+        }
+        this._x = value;
     }
 }
 
 let point = new Point(3,4);
+point.setX = 199;
+console.log(point.getX); // correct <> getX() wrong
+
 point.drawPoint();
